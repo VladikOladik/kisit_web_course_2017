db1956a4ed6482a7652bc8e45b28c62a62617815
Vladislav Red'ko <steamvladikankok@gmail.com>
2018-02-06 11:52:53 +0200

    Add files via upload

---
+++ Lab3/myapp/forms.js
@@ -0,0 +1,44 @@
+var i = 1;
+var availableForms = [{
+		id : i++,
+		name : 'default form which asks to enter name',
+		fields : [{
+			id : 'name',
+			type : 'string'
+		}]
+	}],
+	submittedForms = [];
+
+module.exports = {
+	getAvailableForms : function(){
+		return availableForms;
+	},
+	getForm : function(id){
+		for(let i = 0; i < availableForms.length; i++){
+			if(availableForms[i].id == id) {
+				return availableForms[i]
+			}
+		}
+	},
+	submitForm : function(req) {
+		var id = req.param('formID'),
+			form = this.getForm(id),
+			formObj = {};
+		for(let i = 0; i < form.fields.length; i++){
+			formObj[form.fields[i].id] = req.param(form.fields[i].id);
+		}
+		
+		submittedForms.push({id : id, result : formObj});
+		console.log(submittedForms);
+	},
+	getSubmittedForms : function(){
+		return submittedForms;
+	},
+	getSubmittedForm : function(id){
+		for(let i = 0; i < submittedForms.length; i++){
+			if(submittedForms[i].id == id) {
+				return submittedForms[i]
+			}
+		}
+	}
+};
---
+++ Lab3/myapp/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var forms = require('./../forms');
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	
+  res.render('index', { 
+		title: 'My forms',
+		availableForms :forms.getAvailableForms()
+	});
+});
+
+router.get('/admin', function(req, res){
+	res.render('admin', {
+		title: 'Completed forms',
+		submittedForms: forms.getSubmittedForms()
+	})
+})
+
+router.get('/fillForm', function(req, res){
+	res.render('fillform', {
+		form : forms.getForm(req.param('formID'))
+	});
+});
+
+router.post('/sendForm', urlencodedParser, function(req, res){
+	forms.submitForm(req);
+	res.send('ok <a href="/">home</a>')
+});
+
+router.get('/admin/getSubmittedForm', function(req, res){
+	res.render('checkSubmittedForm', {
+		form : forms.getForm(req.param('formID'))
+	})
+})
+
+module.exports = router;
